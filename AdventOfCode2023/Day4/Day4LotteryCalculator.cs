be04446da97337f23aeb859701735bef8862581a
Stephen Brown <steve@brown.bg>
2023-12-04 21:07:52 +0100

    Day 4 part 2

--- AdventOfCode2023/Day4/Day4LotteryCalculator.cs
+++ AdventOfCode2023/Day4/Day4LotteryCalculator.cs
@@ -50,6 +50,53 @@ public static class Day4LotteryCalculator
     }
 }
 
+public static class Day4Part2LotteryCalculator
+{
+    public static int CalculatePoints(string input)
+    {
+        var cards = input.Split(Environment.NewLine).Select(l => ParseToCard(l)).ToList();
+        var countArray = cards.Select(_ => 1).ToList();
+
+        for (int i = 0; i < cards.Count; i++)
+        {
+            var ownPoints = countArray[i];
+            Card? card = cards[i];
+            var points = 0;
+            foreach (var number in card.ActualNumbers)
+            {
+                if (card.WinningNumbers.Contains(number))
+                {
+                    points++;
+                }
+            }
+            for (int j = 1; j <= points; j++)
+            {
+                countArray[i + j] += ownPoints;
+            }
+            
+        }
+
+        return countArray.Sum();
+    }
+
+    private static Card ParseToCard(string l)
+    {
+        var card = new Card();
+
+        var cardText = l.Split(":");
+        var cardString = cardText[0];
+        var winningString = cardText[1].Split("|")[0];
+        var actualString = cardText[1].Split("|")[1];
+        var winningNumbers = winningString.Split(" ").Where(n => !string.IsNullOrEmpty(n)).Select(n => int.Parse(n.Trim())).ToList();
+        var actualNumbers = actualString.Split(" ").Where(n => !string.IsNullOrEmpty(n)).Select(n => int.Parse(n.Trim())).ToList();
+
+        card.WinningNumbers = winningNumbers;
+        card.ActualNumbers = actualNumbers;
+
+        return card;
+    }
+}
+
 public class Card()
 {
     public List<int> WinningNumbers = [];
